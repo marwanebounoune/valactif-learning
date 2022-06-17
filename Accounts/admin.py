391f1f956468e3bb5f0cca29fe5dda4d164a7576
marwanebounoune <61358019+marwanebounoune@users.noreply.github.com>
2022-06-17 18:13:42 +0100

    v0

--- Accounts/admin.py
+++ Accounts/admin.py
@@ -4,6 +4,8 @@ from django.contrib.auth.admin import UserAdmin as origin
 
 #enregistrement du model User
 class UserAdmin(origin):
-    list_display =  ('id', 'username', 'tel1','tel2','adresse')
-    add_fieldsets = origin.add_fieldsets + ((None, {'fields': ('tel1','tel2','adresse','photoProfile')}),)
+    list_display =  ('id', 'username', 'tel1','adresse', 'dateNaissance', 'genre', 'pays', 'ville', 'lecons', 'url_linkedIn')
+    add_fieldsets = origin.add_fieldsets + ((None, {'fields': ('tel1','adresse', 'dateNaissance', 'url_linkedIn', 'genre', 'pays', 'ville', 'lecons','photoProfile')}),)
 
+
+admin.site.register(User, UserAdmin)
--- Accounts/models.py
+++ Accounts/models.py
@@ -1,18 +1,39 @@
-from distutils.command.upload import upload
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import FileExtensionValidator
-from storages.backends.s3boto3 import S3Boto3Storage
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+GENRE_CHOICES=(
+    ('1', 'Femme'),
+    ('2', 'Homme')
+)
 
+class Pays(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    def __str__(self):
+        return str(self.name)
+    class Meta:
+        db_table = "Pays" 
+
+class Villes(models.Model):
+    name = models.CharField(max_length=50, null=True, blank=True)
+    pays = models.ForeignKey(Pays, null=True, blank=True, on_delete=models.CASCADE)
+    def __str__(self):
+        return str(self.name)
+    class Meta:
+        db_table = "Villes" 
 
 class User(AbstractUser):
     photoProfile = models.FileField(upload_to='images/profiles/',null=True, blank=True, validators=[FileExtensionValidator(allowed_extensions=['png','jpeg','jpg','tiff'])])
     tel1 = models.CharField(max_length=10, null=True, blank=True)
     dateNaissance = models.CharField(max_length=10, null=True, blank=True)
     adresse = models.CharField(max_length=500, null=True, blank=True)
-    url = models.CharField(max_length=500, null=True, blank=True)
+    url_linkedIn = models.CharField(max_length=500, null=True, blank=True)
+    genre = models.CharField(max_length=1, null=True, blank=True, choices=GENRE_CHOICES)
+    pays = models.ForeignKey(Pays, on_delete=models.CASCADE, null=True, blank=True)
+    ville = models.ForeignKey(Villes, on_delete=models.CASCADE, null=True, blank=True)
+    lecons = ArrayField(ArrayField(ArrayField(models.IntegerField(null=True)), null=True, blank=True),null=True, blank=True)
     pass
     class Meta:
         db_table = "User" 
